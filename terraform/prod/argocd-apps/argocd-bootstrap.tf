3e88e7fa25bbb215673cde11d398c7d42d8255fe
wonny <212clab@gmail.com>
2025-12-30 12:31:42 +0900

    Merge branch 'main' into claude/argocd-auto-deploy-dev-tWRSt

--- terraform/prod/argocd-apps/argocd-bootstrap.tf
+++ terraform/prod/argocd-apps/argocd-bootstrap.tf
@@ -24,6 +24,10 @@ resource "kubernetes_namespace" "wealist_prod" {
 # ArgoCD Project 생성 (wealist-prod)
 # -----------------------------------------------------------------------------
 resource "kubernetes_manifest" "argocd_project_prod" {
+  field_manager {
+    force_conflicts = true
+  }
+
   manifest = {
     apiVersion = "argoproj.io/v1alpha1"
     kind       = "AppProject"
@@ -41,6 +45,7 @@ resource "kubernetes_manifest" "argocd_project_prod" {
         "https://charts.jetstack.io",
         "https://kubernetes.github.io/autoscaler",
         "https://kubernetes-sigs.github.io/external-dns",
+        "https://kubernetes-sigs.github.io/metrics-server",
         "https://istio-release.storage.googleapis.com/charts"
       ]
       destinations = [
@@ -111,6 +116,16 @@ resource "kubernetes_manifest" "argocd_project_prod" {
         {
           group = "gateway.networking.k8s.io"
           kind  = "*"
+        },
+        # Metrics Server requires APIService
+        {
+          group = "apiregistration.k8s.io"
+          kind  = "APIService"
+        },
+        # AWS Load Balancer Controller requires elbv2 CRDs
+        {
+          group = "elbv2.k8s.aws"
+          kind  = "*"
         }
       ]
       namespaceResourceWhitelist = [
@@ -144,6 +159,87 @@ resource "kubernetes_manifest" "argocd_root_app" {
       destination = {
         server    = "https://kubernetes.default.svc"
         namespace = "argocd"
+      }
+      syncPolicy = {
+        automated = {
+          prune    = true
+          selfHeal = true
+        }
+        syncOptions = [
+          "CreateNamespace=true"
+        ]
+      }
+    }
+  }
+
+  depends_on = [
+    kubernetes_manifest.argocd_project_prod,
+    kubernetes_namespace.wealist_prod
+  ]
+}
+
+# -----------------------------------------------------------------------------
+# Wealist Infrastructure Application
+# -----------------------------------------------------------------------------
+# Git에서 관리하지 않고 Terraform에서 직접 생성
+# 이유: RDS/ElastiCache 호스트 정보를 Terraform outputs에서 주입해야 함
+# -----------------------------------------------------------------------------
+resource "kubernetes_manifest" "wealist_infrastructure" {
+  field_manager {
+    force_conflicts = true
+  }
+
+  manifest = {
+    apiVersion = "argoproj.io/v1alpha1"
+    kind       = "Application"
+    metadata = {
+      name      = "wealist-infrastructure-prod"
+      namespace = "argocd"
+      annotations = {
+        "argocd.argoproj.io/sync-wave" = "0"
+      }
+    }
+    spec = {
+      project = "wealist-prod"
+      source = {
+        repoURL        = var.git_repo_url
+        targetRevision = var.git_target_revision
+        path           = "k8s/helm/charts/wealist-infrastructure"
+        helm = {
+          valueFiles = [
+            "values.yaml",
+            "../../environments/base.yaml",
+            "../../environments/prod.yaml"
+          ]
+          # RDS/ElastiCache 호스트 정보 주입 (Terraform outputs)
+          values = yamlencode({
+            postgres = {
+              external = {
+                host = data.terraform_remote_state.foundation.outputs.rds_address
+              }
+            }
+            redis = {
+              external = {
+                host = data.terraform_remote_state.foundation.outputs.redis_endpoint
+              }
+            }
+            # Monitoring exporters도 동일한 호스트 사용
+            postgresExporter = {
+              config = {
+                host = data.terraform_remote_state.foundation.outputs.rds_address
+              }
+            }
+            redisExporter = {
+              config = {
+                host = data.terraform_remote_state.foundation.outputs.redis_endpoint
+              }
+            }
+          })
+        }
+      }
+      destination = {
+        server    = "https://kubernetes.default.svc"
+        namespace = "wealist-prod"
       }
       syncPolicy = {
         automated = {
